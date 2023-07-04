be6cf4e34e2008fae8e33aa59d624094d013a401
asarvarjon <amriyevsarvarjon@yandex.com>
2023-07-04 07:22:08 +0500

    fix null checks

--- controllers/vacancyController.js
+++ controllers/vacancyController.js
@@ -25,7 +25,7 @@ module.exports.create_vacancy = async (req, res) => {
         description,
         salary,
         name,
-        company_id: res.locals.company?._id
+        company_id: res.locals.company ? res.locals.company._id : null,
     });
     console.log(new_vacancy)
     
